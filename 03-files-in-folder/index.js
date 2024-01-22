6c783dd3b4df0d3c7789434e57c34253429baa7a
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-22 21:35:08 +0100

    refactor: add changes to index.js in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 const secretFolderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(secretFolderPath, { withFileTypes: true }, (error, files) => {
+fs.readdir(secretFolderPath,
+  { withFileTypes: true },
+  (error, files) => {
   if (error) return console.error(error.message);
   const filteredFiles = files.filter((file) => file.isFile());
   filteredFiles.forEach((file) => {
-    fs.stat(path.join(secretFolderPath, file.name), (error, stats) => {
+    fs.stat(path.join(secretFolderPath, file.name),
+    (error, stats) => {
       if (error) return console.error(error.message);
       const name = file.name.split('.')[0];
       const extension = path.extname(file.name).slice(1);
