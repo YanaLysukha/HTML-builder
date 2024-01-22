d54f85fb7103c8ea72ef3ea94d924c0fce6b9445
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-22 19:11:26 +0100

    fix: fix functionality in the copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,14 +6,24 @@ function copyDir(copyFrom, copyTo) {
     if (error) return console.error(error.message);
   });
 
-  fs.readdir(path.join(copyFrom), 'utf8', (error, files) => {
+  fs.readdir(path.join(copyFrom), 'utf8', (error, originalFiles) => {
     if (error) return console.error(error.message);
-    files.forEach((file) => {
+    originalFiles.forEach((file) => {
       const copyFileFrom = path.join(copyFrom, file);
       const copyFileTo = path.join(copyTo, file);
       fs.copyFile(copyFileFrom, copyFileTo, (error) => {
         if (error) return console.error(error.message);
       });
+    })
+    fs.readdir(copyTo, 'utf8', (error, files) => {
+      if (error) return console.error(error.message);
+      for (const file of files) {
+        if (!originalFiles.includes(file)) {
+          fs.rm(path.join(copyTo, file), (error) => {
+            if (error) return console.error(error.message);
+          })
+        }
+      }
     })
   })
 }
