2f136f3dc4b1f380fd021f2b002c0d11a3949398
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-22 13:24:43 +0100

    feat: put script into buildHtml function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -11,28 +11,32 @@ fs.mkdir(projectDistDirectory, { recursive: true }, (error) => {
   if (error) return console.error(error.message);
 });
 
-fs.readFile(templateFile, 'utf8', (error, data) => {
-  if (error) return console.error(error.message);
-  let templateHtmlData = data;
-
-  fs.readdir(componentsDirectory, { withFileTypes: true }, (error, componentFiles) => {
+function buildHtml(originalFile, pageComponentsDir) {
+  fs.readFile(originalFile, 'utf8', (error, data) => {
     if (error) return console.error(error.message);
-    const filteredComponents = componentFiles.filter((file) => path.extname(file.name) === '.html');
-
-    for (const component of filteredComponents) {
-      fs.readFile(path.join(componentsDirectory, component.name), 'utf-8', (error, componentData) => {
-        if (error) return console.error(error.message);
-        const compName = component.name.split('.')[0];
-        templateHtmlData = templateHtmlData.replace(`{{${compName}}}`, componentData.toString());
-
-        fs.writeFile(path.join(projectDistDirectory, 'index.html'), templateHtmlData, (error) => {
+    let templateHtmlData = data;
+  
+    fs.readdir(pageComponentsDir, { withFileTypes: true }, (error, componentFiles) => {
+      if (error) return console.error(error.message);
+      const filteredComponents = componentFiles.filter((file) => path.extname(file.name) === '.html');
+  
+      for (const component of filteredComponents) {
+        fs.readFile(path.join(pageComponentsDir, component.name), 'utf-8', (error, componentData) => {
           if (error) return console.error(error.message);
-          // console.log('File was created');
+          const compName = component.name.split('.')[0];
+          templateHtmlData = templateHtmlData.replace(`{{${compName}}}`, componentData.toString());
+  
+          fs.writeFile(path.join(projectDistDirectory, 'index.html'), templateHtmlData, (error) => {
+            if (error) return console.error(error.message);
+            // console.log('File was created');
+          })
         })
-      })
-    }
+      }
+    })
   })
-})
+}
+
+buildHtml(templateFile, componentsDirectory);
 
 // fs.readdir(stylesDirectory, { withFileTypes: true }, (error, files) => {
 //   if (error) return console.error(error.message);
