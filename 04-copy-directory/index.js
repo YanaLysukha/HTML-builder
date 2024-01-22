767e38d4527eaea424666302a023f0726fa7c2d3
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-22 21:38:04 +0100

    refactor: add changes to index.js in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,24 +2,31 @@ const fs = require('fs');
 const path = require('path');
 
 function copyDir(copyFrom, copyTo) {
-  fs.mkdir(path.join(copyTo), { recursive: true }, (error) => {
+  fs.mkdir(path.join(copyTo),
+  { recursive: true },
+  (error) => {
     if (error) return console.error(error.message);
   });
 
-  fs.readdir(path.join(copyFrom), 'utf8', (error, originalFiles) => {
-    if (error) return console.error(error.message);
-    originalFiles.forEach((file) => {
-      const copyFileFrom = path.join(copyFrom, file);
-      const copyFileTo = path.join(copyTo, file);
-      fs.copyFile(copyFileFrom, copyFileTo, (error) => {
-        if (error) return console.error(error.message);
-      });
-    })
-    fs.readdir(copyTo, 'utf8', (error, files) => {
+  fs.readdir(path.join(copyFrom),
+    'utf8',
+    (error, originalFiles) => {
       if (error) return console.error(error.message);
-      for (const file of files) {
-        if (!originalFiles.includes(file)) {
-          fs.rm(path.join(copyTo, file), (error) => {
+      originalFiles.forEach((file) => {
+        const copyFileFrom = path.join(copyFrom, file);
+        const copyFileTo = path.join(copyTo, file);
+        fs.copyFile(copyFileFrom, copyFileTo, (error) => {
+          if (error) return console.error(error.message);
+        });
+      })
+    fs.readdir(copyTo,
+      'utf8',
+      (error, files) => {
+        if (error) return console.error(error.message);
+        for (const file of files) {
+          if (!originalFiles.includes(file)) {
+            fs.rm(path.join(copyTo, file),
+            (error) => {
             if (error) return console.error(error.message);
           })
         }
